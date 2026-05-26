d5c8ff74c59348c5e3bb8f045572342890339196
Aftab Sk <ska388017@gmail.com>
2026-05-26 18:24:09 +0000

    Remove old ad network scripts completely.
    Keep only Adsterra Social Bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
+import Script from 'next/script';
 
 // Cache busting version
-const APP_VERSION = "2.5.1";
+const APP_VERSION = "2.5.2";
 
 export const metadata: Metadata = {
   title: 'FarmRush - Neon Cyber Mining',
@@ -21,23 +22,29 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-        {/* Telegram WebApp SDK with Cache Busting */}
-        <script src={`https://telegram.org/js/telegram-web-app.js?v=${APP_VERSION}`} async></script>
-        {/* Monetag SDK with Cache Busting */}
-        <script src={`//libtl.com/sdk.js?v=${APP_VERSION}`} data-zone='11042868' data-sdk='show_11042868' async></script>
       </head>
       <body className="font-body antialiased selection:bg-primary selection:text-white overflow-x-hidden w-screen min-h-screen">
+        {/* Telegram WebApp SDK */}
+        <Script 
+          src={`https://telegram.org/js/telegram-web-app.js?v=${APP_VERSION}`} 
+          strategy="beforeInteractive" 
+        />
+
         <FirebaseClientProvider>
           {children}
         </FirebaseClientProvider>
 
-        {/* Adsterra Monetization - Website ID: 5808596 */}
-        
-        {/* Social Bar Integration (ID: 29460513) */}
-        <script type='text/javascript' src='//pl29460513.highratecpm.com/ec/2e/9a/ec2e9ab4e9f78367d26e4e5b5e5e5e5e.js' async></script>
+        {/* Adsterra Social Bar Integration (ID: 29460513) */}
+        <Script
+          src="//pl29460513.highratecpm.com/ec/2e/9a/ec2e9ab4e9f78367d26e4e5b5e5e5e5e.js"
+          strategy="afterInteractive"
+        />
 
-        {/* Popunder Integration (ID: 29460514) */}
-        <script type='text/javascript' src='//pl29460514.highratecpm.com/71/82/e3/7182e3f454f76274472390f05808e002.js' async></script>
+        {/* Adsterra Popunder Integration (ID: 29460514) */}
+        <Script
+          src="//pl29460514.highratecpm.com/71/82/e3/7182e3f454f76274472390f05808e002.js"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
